94abb00d972aaeaee31c2dadb0d4b7e59cebfef2
David <daviddengcn@163.com>
2013-07-13 11:55:40 +0800

    ADD AssertLinesEquals

--- utils.go
+++ utils.go
@@ -6,6 +6,40 @@ import (
 	"testing"
 )
 
+func AssertStringsEquals(t *testing.T, name string, act, exp []string) {
+	t.Errorf("%s unexpected(exp: %d lines, act %d lines!", name, len(exp), len(act))
+	t.Logf("exp ---  act +++")
+	t.Logf("Difference:")
+	_, matA, matB := ed.EditDistanceFFull(len(exp), len(act), func(iA, iB int) int {
+		sa, sb := exp[iA], act[iB]
+		if sa == sb {
+			return 0
+		}
+		return ed.String(sa, sb)
+	}, func(iA int) int {
+		return len(exp[iA]) + 1
+	}, func(iB int) int {
+		return len(act[iB]) + 1
+	})
+	for i, j := 0, 0; i < len(exp) || j < len(act); {
+		switch {
+		case j >= len(act) || i < len(exp) && matA[i] < 0:
+			t.Logf("--- %3d: %s", i+1, showText(exp[i]))
+			i++
+		case i >= len(exp) || j < len(act) && matB[j] < 0:
+			t.Logf("+++ %3d: %s", j+1, showText(act[j]))
+			j++
+		default:
+			if exp[i] != act[j] {
+				t.Logf("--- %3d: %s", i+1, showText(exp[i]))
+				t.Logf("+++ %3d: %s", j+1, showText(act[j]))
+			} // else
+			i++
+			j++
+		}
+	} // for i, j
+}
+
 func showText(text string) string {
 	return text + "."
 }
@@ -17,37 +51,7 @@ func AssertTextEquals(t *testing.T, name, act, exp string) {
 
 	expLines := strings.Split(exp, "\n")
 	actLines := strings.Split(act, "\n")
-
-	t.Errorf("%s unexpected(exp: %d lines, act %d lines!", name, len(expLines), len(actLines))
-	t.Logf("exp ---  act +++")
-	t.Logf("Difference:")
-	_, matA, matB := ed.EditDistanceFFull(len(expLines), len(actLines), func(iA, iB int) int {
-		sa, sb := expLines[iA], actLines[iB]
-		if sa == sb {
-			return 0
-		}
-		return ed.String(sa, sb)
-	}, func(iA int) int {
-		return len(expLines[iA]) + 1
-	}, func(iB int) int {
-		return len(actLines[iB]) + 1
-	})
-	for i, j := 0, 0; i < len(expLines) || j < len(actLines); {
-		switch {
-		case j >= len(actLines) || i < len(expLines) && matA[i] < 0:
-			t.Logf("--- %3d: %s", i+1, showText(expLines[i]))
-			i++
-		case i >= len(expLines) || j < len(actLines) && matB[j] < 0:
-			t.Logf("+++ %3d: %s", j+1, showText(actLines[j]))
-			j++
-		default:
-			if expLines[i] != actLines[j] {
-				t.Logf("--- %3d: %s", i+1, showText(expLines[i]))
-				t.Logf("+++ %3d: %s", j+1, showText(actLines[j]))
-			} // else
-			i++
-			j++
-		}
-	} // for i, j
+	
+	AssertStringsEquals(t, name, actLines, expLines)
 }
 
